ce0adf1bd9019de50f85996930e9277f6890838b
Kasper <kasper.bjorkgren@gmail.com>
2022-03-30 00:03:16 +0300

    Added ball

--- Assets/Scripts/ShellBehaviour.cs
+++ Assets/Scripts/ShellBehaviour.cs
@@ -6,9 +6,33 @@ public class ShellBehaviour : MonoBehaviour
 {
     [SerializeField]
     GameObject explosion;
-    private void OnCollisionEnter(Collision collision)
+    public float followSpeed;
+    private void Update()
     {
+        transform.LookAt(GameObject.Find("Ball").transform);
+
+        Vector3 followDir = Vector3.forward;
+
+        transform.Translate(followDir * followSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Bounds")
+        {
+            return;
+        }
+
         Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Vector3 direction = (other.transform.position - transform.position).normalized;
+
+            direction.y = Mathf.Clamp(direction.y, 0, 1);
+
+            other.gameObject.GetComponent<Rigidbody>().AddForce(direction * 30, ForceMode.Impulse);
+        }
 
         Destroy(gameObject);
     }
